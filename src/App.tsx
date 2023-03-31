cf257a9cf7a6abc6c4050792ba89f8edd3aa9725
FelixT <felixtekhada@gmail.com>
2023-03-31 19:37:58 +0200

    added datepicker from antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {DatePicker} from 'antd';
+// import 'antd/dist/antd.css';
+import {useState} from 'react';
+// import moment from 'moment';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [dates, setDates] = useState<any>()
+
+
+    return (
+        <>
+
+            <div style={{margin:'20px 0 20px 5px'}}>
+
+
+                < DatePicker
+                    onChange={(values: any) => {
+                        setDates(values?.toLocaleString())
+                    }}
+                />
+            </div>
+            <div>    {dates && <><b>RETURN</b> = {dates}</>}</div>
+        </>
+    );
 }
 
 export default App;
